9e07ab6f51a280c907754670f8095a9d858d5567
Daniel Dias <lupeke@gmail.com>
2012-09-14 21:45:06 -0300

    validate user input

--- web.rb
+++ web.rb
@@ -8,24 +8,33 @@ get '/' do
 end
 
 post '/verify' do
-  email = params[:email]
-  id, host = email.split('@')
-  mx = Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN::MX).map { |dns| dns.exchange.to_s }
-
   response = []
-  smtp = Net::SMTP.start(mx[0], 25)
-  response << smtp.helo('verify-email.io').string
-  response << smtp.mailfrom('bot@verify-email.io').string
-  begin
-    response << smtp.rcptto(email).string
-  rescue Net::SMTPFatalError => e
-    response << e.message
-  end
-  smtp.finish
+  email = params[:email]
+
+  if valid?(email)
+    id, host = email.split('@')
+    mx = Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN::MX).map { |dns| dns.exchange.to_s }
+
+    smtp = Net::SMTP.start(mx[0], 25)
+    response << smtp.helo('verify-email.io').string
+    response << smtp.mailfrom('bot@verify-email.io').string
+    begin
+      response << smtp.rcptto(email).string
+    rescue Net::SMTPFatalError => e
+      response << e.message
+    end
+    smtp.finish
+  else
+    response << "invalid email"
+  end  
 
   response.each do |response|
     response
   end
 
   erb :verify, :locals => { :response => response }
+end
+
+def valid?(email)
+  /^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/ =~ email
 end
